a89af240db59cf90a47c0cedfa3f7aa8b70616aa
Misa24Jr <misa24jr@gmail.com>
2024-03-06 18:16:13 -0400

    Cambios

--- src/app/views/new-post-view/new-post-view.page.ts
+++ src/app/views/new-post-view/new-post-view.page.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
-import { Storage, ref, uploadBytes } from '@angular/fire/storage'
+import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fire/storage'
 
 @Component({
   selector: 'app-new-post-view',
@@ -12,9 +12,13 @@ import { Storage, ref, uploadBytes } from '@angular/fire/storage'
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule, RouterLink],
 })
+
 export class NewPostViewPage implements OnInit {
+  images: string[];
   textInput: string;
   characters: any;
+  countImg: number;
+
 
   uploadImage(event: any){
     const file = event.target.files[0];
@@ -32,12 +36,34 @@ export class NewPostViewPage implements OnInit {
   constructor(private storage: Storage) { 
     this.textInput = '';
     this.characters = '';
+    this.images = [];
+    this.countImg = 0;
+
    }
 
   ngOnInit() {
+    this.getImages();
   }
 
   countCharacters(){
     this.characters = this.textInput.length;
+  }
+
+  getImages(){
+    const imgRef = ref(this.storage, 'images');
+
+    listAll(imgRef)
+    .then(async response => {
+        //console.log(response);
+        this.images = [];
+        for(let item of response.items){
+          const url = await getDownloadURL(item)
+          this.images.push(url);
+          // console.log(url);
+        }
+        this.countImg = this.images.length; // Asignar el número de imágenes a la variable countImg
+        console.log(this.countImg);
+    })
+    .catch(error => console.log(error));
   }
 }
