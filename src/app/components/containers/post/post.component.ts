8ba8ac14302f8fc946f2a0cbbd594bc076567157
Misa24Jr <misa24jr@gmail.com>
2024-03-06 15:21:50 -0400

    Cambios en el front

---
+++ src/app/components/containers/post/post.component.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+import { Storage, ref } from '@angular/fire/storage';
+import { listAll, getDownloadURL } from '@firebase/storage';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export class PostComponent  implements OnInit {
+   images: string[];
+   likeClicked: boolean;
+   favoeiteClicked: boolean;
+   @Input() description:string = '';
+    @Input() name: string = '';
+    @Input() date: string = '';
+
+  constructor(private storage: Storage) { 
+    this.images = [];
+    this.likeClicked = false;
+    this.favoeiteClicked = false;
+   }
+
+  ngOnInit() { 
+    this.getImages();
+    // this.getFriendPosts();
+   }
+
+   onLikeClick() {
+    this.likeClicked = !this.likeClicked;
+    // this.favoeiteClicked = false;
+  }
+  onFavoriteClick() {
+    this.favoeiteClicked = !this.favoeiteClicked;
+    // this.likeClicked = false;
+  }
+
+  getImages(){
+    const imgRef = ref(this.storage, 'images');
+
+    listAll(imgRef)
+    .then(async response => {
+        //console.log(response);
+        this.images = [];
+        for(let item of response.items){
+          const url = await getDownloadURL(item)
+          this.images.push(url);
+          // console.log(url);
+        }
+    })
+    .catch(error => console.log(error));
+  }
+
+}
---
+++ src/app/views/new-post-view/new-post-view.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
+import { Storage, ref, uploadBytes } from '@angular/fire/storage'
+
+@Component({
+  selector: 'app-new-post-view',
+  templateUrl: './new-post-view.page.html',
+  styleUrls: ['./new-post-view.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
+})
+export class NewPostViewPage implements OnInit {
+  textInput: string;
+  characters: any;
+
+  uploadImage(event: any){
+    const file = event.target.files[0];
+    console.log(file);
+
+
+    const imgRef = ref(this.storage, `images/${file.name}`);
+
+    uploadBytes(imgRef, file)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+
+  }
+  constructor(private storage: Storage) { 
+    this.textInput = '';
+    this.characters = '';
+   }
+
+  ngOnInit() {
+  }
+
+  countCharacters(){
+    this.characters = this.textInput.length;
+  }
+}
